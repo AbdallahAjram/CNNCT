9a79c8a0eb4bb53e22261fbcb82d5f2bd28ba36c
Mohamad Baker Saleh <mohamadbakerhassan3@gmail.com>
2025-07-14 19:51:45 +0300

    Initial commit

---
+++ a_core/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for a_core project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import URLRouter
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+django_asgi_app = get_asgi_application()
+from a_rtchat import routing
+application=ProtocolTypeRouter({
+    "http":django_asgi_app,
+    "websocket":AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    )
+})
---
+++ a_rtchat/models.py
@@ -0,0 +1,19 @@
+# a_rtchat/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class ChatGroup(models.Model):
+    group_name = models.CharField(max_length=100, unique=True)
+    users_online = models.ManyToManyField(User, related_name='online_in_groups',blank=True)
+    
+    def __str__(self):  
+        return self.group_name
+class GroupMessages(models.Model):
+    group=models.ForeignKey(ChatGroup,related_name='chat_messages', on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    body= models.CharField(max_length=300)
+    created= models.DateField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.author.username} : {self.body}'  
+    class Meta:
+        ordering=['-created'] 
